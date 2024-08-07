7770898e99258e79c937e0b2bafe23f6b0cb0e5e
Manmeetkour12 <100256617+Manmeetkour12@users.noreply.github.com>
2024-08-07 15:50:59 +0530

    Update Longest Valid Parentheses.cpp

--- Longest Valid Parentheses.cpp
+++ Longest Valid Parentheses.cpp
@@ -1,3 +1,5 @@
+//first approach
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
@@ -22,4 +24,55 @@ public:
     return maxlen;
     }
 };
+
+//second approach
+
+class Solution {
+public:
+    
+
+int longestValidParentheses(string s){
+     
+int n=s.length();
+int count1=0;
+int count2=0;
+int max1=0;
+int max2=0;
+for(int i=0;i<s.length();i++){
+    if(s[i]=='('){
+        count1++;
+    }
+    else if(s[i]==')'){
+        count2++;
+    }
+    if(count1==count2){
+        max1=max(max1,count1+count2);
+    }
+    else if(count1 < count2){
+        count1=0;
+        count2=0;
+    }
+}
+    count1=count2=0;
+    for(int i=s.length()-1;i>=0;i--){
+    if(s[i]=='('){
+        count1++;
+    }
+    else if(s[i]==')'){
+        count2++;
+    }
+    if(count2==count1){
+        max2=max(max2,count2+count1);
+    }
+    else if(count1 > count2){
+        count1=0;
+        count2=0;
+    }
+    }
+    int result;
+    result = max(max1, max2);
+      return result;
+    }
+};
+
 
