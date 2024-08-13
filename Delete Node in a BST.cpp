44ecfe529b58f50db69db081d5ab8a5acb16198f
Manmeetkour12 <100256617+Manmeetkour12@users.noreply.github.com>
2024-08-13 15:40:22 +0530

    Update Delete Node in a BST.cpp

--- Delete Node in a BST.cpp
+++ Delete Node in a BST.cpp
@@ -74,3 +74,39 @@ public:
   }
 
 };
+
+//2nd approach
+class Solution {
+public:
+TreeNode* deleteNode(TreeNode* root, int key){
+    if(root == NULL)
+        return root;
+    if(key < root->val)
+        root->left = deleteNode(root->left,key);
+    else if(key>root->val)
+         root->right = deleteNode(root->right,key);
+    else{
+        if(root->left == NULL && root->right == NULL)
+            return NULL;
+        else if(root->left != NULL && root->right == NULL)
+            return root->left;
+        else if(root->left == NULL && root->right != NULL)
+            return root->right;
+        else{
+            root->val = getMax(root->left);
+            root->left = deleteNode(root->left,root->val);
+
+        }
+    }
+    return root;
+    }
+    int getMax(TreeNode* node){
+        int max = node->val;
+        while (node->right != NULL) {
+            max = node->right->val;
+            node = node->right;
+        }
+        return max;
+
+    }
+};
